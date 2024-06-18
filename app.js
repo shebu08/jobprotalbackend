4290491e52c1ffcf7990e7f482e58cbc23453293
shebu08 <shariq.jhs@gmail.com>
2024-06-18 22:09:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import jobRouter from "./routes/jobRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import applicationRouter from "./routes/applicationRoutes.js";
+import dotenv from "dotenv";
+
+import cors from "cors";
+import { errorMiddleware } from "./middlewares/error.js";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+
+const app = express();
+dotenv.config({ path: "./config.env" });
+
+app.use(
+  cors({
+    origin: [process.env,"http://localhost:5174/login"],
+    method: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(errorMiddleware);
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/job", jobRouter);
+app.use("/api/v1/application", applicationRouter);
+dbConnection();
+
+export default app;
